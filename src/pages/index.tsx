764c85f294976df20864547db7c51a5c5183e7e1
syahirdev <888syahir@gmail.com>
2022-07-14 15:24:27 +0800

    add pagination

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,47 @@
 import axios from "axios";
-import Image from "next/image";
 import Card from "../components/Card";
+import { GetServerSideProps } from "next";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 type Props = {
-  data: ResponseInterface
+  data: ResponseInterface,
+  page: number
 }
 
-export default function Home({ data }: Props) {
+export default function Home({ data, page }: Props) {
+  // STATES
+  const [currentPage, setCurrentPage] = useState(page);
+
+  // HOOKS
+  const router = useRouter();
+
+  // FUNCTIONS
+  const onChangePage = (pageNumber: number) => {
+    const updatedPageNumber = pageNumber <= 0 ? 1 : pageNumber;
+    setCurrentPage(updatedPageNumber);
+
+    router.push({
+      pathname: router.pathname,
+      query: {
+        page: updatedPageNumber
+      }
+    });
+  };
+
+  // VIEWS
   return (
     <main>
+      <div className="flex gap-x-2">
+        <div onClick={() => onChangePage(page - 1)}>prev</div>
+        <input
+          name="page"
+          type="number"
+          value={currentPage}
+          onChange={(e) => onChangePage(parseInt(e.target.value))}
+        />
+        <div onClick={() => onChangePage(page + 1)}>next</div>
+      </div>
       <div className="flex flex-col gap-y-3">
         {data.results?.map((user, index) => (
           <Card key={index} user={user}/>
@@ -18,11 +51,14 @@ export default function Home({ data }: Props) {
   );
 }
 
-export async function getServerSideProps() {
-  const { data } = await axios.get("https://randomuser.me/api/?seed=lll&page=1&results=25");
+export const getServerSideProps: GetServerSideProps = async({ query }) => {
+  const page = parseInt(query.page as string) || 1;
+  const { data } = await axios.get(`https://randomuser.me/api/?seed=lll&page=${page}&results=25`);
+
   return {
     props: {
-      data
+      data,
+      page
     }
   };
-}
+};
